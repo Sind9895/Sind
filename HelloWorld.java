c5f058b0e2a7be7cb1a373df99abdcbbd7db1603
Sind9895 <145627094+Sind9895@users.noreply.github.com>
2023-09-20 18:09:56 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package firstassignment;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+     //Prints Hello World
+     System.out.println("Hello World");
+    }
+}
